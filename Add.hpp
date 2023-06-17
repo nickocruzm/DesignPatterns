b2735d5b6f44522bf86577947e848d07da973ade
Nicko Cruz M <nickocruzm@gmail.com>
2023-06-16 19:04:04 -0700

    File Transferred from School account

---
+++ Add.hpp
@@ -0,0 +1,33 @@
+#ifndef __ADD_HPP__
+#define __ADD_HPP__
+
+#include "base.hpp"
+#include <iostream>
+
+using namespace std;
+
+class Add: public Base{
+    private:
+    
+        Base* x1;
+        Base* x2;
+
+    public:
+        Add(Base* a, Base* b){
+            this->x1 = a;
+            this->x2 = b;
+        }
+
+        double evaluate(){
+            return (x1->evaluate() + x2->evaluate());
+        }
+
+        string stringify(){
+            return( "(" + x1->stringify() + "+" + x2->stringify() + ")" );
+        }
+
+};
+
+
+
+#endif
