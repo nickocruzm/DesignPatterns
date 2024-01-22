da9b2dbc4d29379003f063725a412bccb7975ca3
Nicko Cruz M <nickocruzm@gmail.com>
2024-01-21 22:08:06 -0800

    small change

--- visitor.hpp
+++ visitor.hpp
@@ -8,7 +8,6 @@ class Sub;
 class Mult;
 class Div;
 class Pow;
-
 class Visitor{
     public:
         virtual ~Visitor() = default;
@@ -19,15 +18,19 @@ class Visitor{
         virtual void visit_add_begin(Add* node) = 0;
         virtual void visit_add_middle(Add* node) = 0;
         virtual void visit_add_end(Add* node) = 0;
+
         virtual void visit_sub_begin(Sub* node) = 0;
         virtual void visit_sub_middle(Sub* node) = 0;
         virtual void visit_sub_end(Sub* node) = 0;
+
         virtual void visit_mult_begin(Mult* node) = 0;
         virtual void visit_mult_middle(Mult* node) = 0;
         virtual void visit_mult_end(Mult* node) = 0;
+
         virtual void visit_div_begin(Div* node) = 0;
         virtual void visit_div_middle(Div* node) = 0;
         virtual void visit_div_end(Div* node) = 0;
+        
         virtual void visit_pow_begin(Pow* node) = 0;
         virtual void visit_pow_middle(Pow* node) = 0;
         virtual void visit_pow_end(Pow* node) = 0;
